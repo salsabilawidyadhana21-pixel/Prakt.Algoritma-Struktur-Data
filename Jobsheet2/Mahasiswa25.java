9a3c5947dd9251b5b6fd84772405567d9ce7acb8
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-02-24 17:37:04 +0700

    Modifikasi Percobaan 1

--- Jobsheet2/Mahasiswa25.java
+++ Jobsheet2/Mahasiswa25.java
@@ -1,3 +1,4 @@
+
 public class Mahasiswa25 {
     // Atribut 
     String nim;
@@ -7,32 +8,49 @@ public class Mahasiswa25 {
 
     // Method untuk menampilkan informasi mahasiswa 
     void tampilkanInformasi() {
-        System.out.println("Nama: " + nama); 
-        System.out.println("NIM: " + nim); 
-        System.out.println("IPK: " + ipk); 
-        System.out.println("Kelas: " + kelas); 
+        System.out.println("Nama  : " + nama); 
+        System.out.println("NIM   : " + nim); 
+        System.out.println("IPK   : " + ipk); 
+        System.out.println("Kelas : " + kelas); 
     }
 
-    // Method untuk mengubah kelas 
+    // Method untuk mengubah kelas [cite: 49]
     void ubahKelas(String kelasBaru) {
         kelas = kelasBaru; 
     }
 
-    // Method untuk memperbarui nilai IPK 
+    // Method update Ipk dengan MODIFIKASI VALIDASI (Pertanyaan No. 4) 
     void updateIpk(double ipkBaru) {
-        ipk = ipkBaru; 
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) { 
+            ipk = ipkBaru; 
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0"); 
+        }
     }
 
     // Method untuk mengevaluasi kinerja berdasarkan IPK 
     String nilaiKinerja() {
         if (ipk >= 3.5) { 
-            return "Kinerja sangat baik";
+            return "Kinerja sangat baik"; 
         } else if (ipk >= 3.0) { 
             return "Kinerja baik"; 
         } else if (ipk >= 2.0) { 
             return "Kinerja cukup"; 
-        } else { 
+        } else {
             return "Kinerja kurang"; 
         }
+    }
+
+    // Fungsi main untuk menjalankan program
+    public static void main(String[] args) {
+        // Instansiasi objek mhs 
+        Mahasiswa25 mhs = new Mahasiswa25(); 
+        
+        // Mengisi atribut objek 
+        mhs.nama = "Salsa"; 
+        mhs.nim = "254107020200"; 
+        
+        // Memanggil method untuk menampilkan informasi 
+        mhs.tampilkanInformasi(); 
     }
 }
