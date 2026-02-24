b974a8bb0b338183a6a98c42170423616f166985
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-02-24 19:07:17 +0700

    Pertanyaan No.5 Percobaan 3

--- Jobsheet2/MahasiswaMain25.java
+++ Jobsheet2/MahasiswaMain25.java
@@ -1,23 +1,29 @@
 public class MahasiswaMain25 {
     public static void main(String[] args) {
-        // Objek mhs1 menggunakan konstruktor default (Percobaan 2)
-        Mahasiswa25 mhs1 = new Mahasiswa25();
+        
+        // --- DATA MAHASISWA 1 (Konstruktor Default) ---
+        Mahasiswa25 mhs1 = new Mahasiswa25(); 
         mhs1.nama = "Muhammad Ali Farhan";
         mhs1.nim = "2241720171";
         mhs1.kelas = "SI 2J";
         mhs1.ipk = 3.55;
-
-        mhs1.tampilkanInformasi();
-        mhs1.ubahKelas("SI 2K");
-        mhs1.updateIpk(3.60);
-        mhs1.tampilkanInformasi();
-
-        // [Percobaan 3 - Langkah 2] 
-        // Membuat objek mhs2 menggunakan konstruktor berparameter
-        Mahasiswa25 mhs2 = new Mahasiswa25("Annisa Nabila", "2141720160", 3.25, "TI 2L"); 
         
-        // Memanggil method updateIpk dan tampilkanInformasi untuk mhs2
-        mhs2.updateIpk(3.30); 
-        mhs2.tampilkanInformasi(); 
+        System.out.println("=== DATA MAHASISWA 1 ===");
+        mhs1.tampilkanInformasi();
+
+        // --- DATA MAHASISWA 2 (Konstruktor Berparameter) ---
+        Mahasiswa25 mhs2 = new Mahasiswa25("Annisa Nabila", "2141720160", 3.25, "TI 2L");
+        
+        System.out.println("\n=== DATA MAHASISWA 2 ===");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
+
+        // --- JAWABAN NOMOR 5 (Objek Salsabila Widyadhana) ---
+        // Menggunakan konstruktor berparameter
+        Mahasiswa25 mhsSalsabila = new Mahasiswa25("Salsabila Widyadhana", "2441720025", 3.95, "TI 1H");
+        
+        System.out.println("\n=== DATA MAHASISWA SALSABILA WIDYADHANA ===");
+        mhsSalsabila.tampilkanInformasi();
+        System.out.println("Nilai Kinerja: " + mhsSalsabila.nilaiKinerja());
     }
 }
