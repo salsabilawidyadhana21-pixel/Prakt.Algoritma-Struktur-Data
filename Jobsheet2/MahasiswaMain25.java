5c12fdde9f943a98cadb308270f9ee9fb5576307
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-02-24 18:32:00 +0700

    Percobaan 3 No.2

--- Jobsheet2/MahasiswaMain25.java
+++ Jobsheet2/MahasiswaMain25.java
@@ -1,23 +1,23 @@
-
 public class MahasiswaMain25 {
     public static void main(String[] args) {
-        // 1. Instansiasi objek mhs1 menggunakan konstruktor default 
+        // Objek mhs1 menggunakan konstruktor default (Percobaan 2)
         Mahasiswa25 mhs1 = new Mahasiswa25();
-
-        // 2. Mengisi atribut mhs1 
         mhs1.nama = "Muhammad Ali Farhan";
         mhs1.nim = "2241720171";
         mhs1.kelas = "SI 2J";
         mhs1.ipk = 3.55;
 
-        // 3. Memanggil method tampilkanInformasi pertama kali 
         mhs1.tampilkanInformasi();
-
-        // 4. Mengubah data menggunakan method ubahKelas dan updateIpk 
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
-
-        // 5. Menampilkan informasi kembali untuk melihat perubahan 
         mhs1.tampilkanInformasi();
+
+        // [Percobaan 3 - Langkah 2] 
+        // Membuat objek mhs2 menggunakan konstruktor berparameter
+        Mahasiswa25 mhs2 = new Mahasiswa25("Annisa Nabila", "2141720160", 3.25, "TI 2L"); 
+        
+        // Memanggil method updateIpk dan tampilkanInformasi untuk mhs2
+        mhs2.updateIpk(3.30); 
+        mhs2.tampilkanInformasi(); 
     }
 }
