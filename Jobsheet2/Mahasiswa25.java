f0d75e307bef4758a0bae7a8283ee07ec45a8834
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-02-24 18:49:12 +0700

    Pertanyaan No.3 Percobaan3

--- Jobsheet2/Mahasiswa25.java
+++ Jobsheet2/Mahasiswa25.java
@@ -1,23 +1,20 @@
 public class Mahasiswa25 {
-    // Atribut 
     String nama;
     String nim;
     String kelas;
     double ipk;
 
-    // Konstruktor Default 
-    public Mahasiswa25() {
-    }
+    // KONSTRUKTOR DEFAULT DIHAPUS (Pertanyaan No. 3 Percobaan 3)
 
-    // Konstruktor Berparameter 
+
+    // Konstruktor Berparameter
     public Mahasiswa25(String nm, String nim, double ipk, String kls) {
-        this.nama = nm;  // Menggunakan 'this' jika nama parameter sama dengan atribut 
+        this.nama = nm;
         this.nim = nim;
         this.ipk = ipk;
         this.kelas = kls;
     }
 
-    // Method untuk menampilkan informasi lengkap mahasiswa 
     void tampilkanInformasi() {
         System.out.println("Nama  : " + nama);
         System.out.println("NIM   : " + nim);
@@ -25,12 +22,10 @@ public class Mahasiswa25 {
         System.out.println("Kelas : " + kelas);
     }
 
-    // Method untuk mengubah kelas 
     void ubahKelas(String kelasBaru) {
         kelas = kelasBaru;
     }
 
-    // Method updateIpk dengan MODIFIKASI VALIDASI (0.0 - 4.0) 
     void updateIpk(double ipkBaru) {
         if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
             ipk = ipkBaru;
@@ -39,41 +34,10 @@ public class Mahasiswa25 {
         }
     }
 
-    // Method untuk mengevaluasi kinerja berdasarkan rentang IPK 
     String nilaiKinerja() {
-        if (ipk >= 3.5) {
-            return "Kinerja sangat baik";
-        } else if (ipk >= 3.0) {
-            return "Kinerja baik";
-        } else if (ipk >= 2.0) {
-            return "Kinerja cukup";
-        } else {
-            return "Kinerja kurang";
-        }
-    }
-
-    // Fungsi main untuk menjalankan seluruh percobaan 
-    public static void main(String[] args) {
-        // Instansiasi menggunakan Konstruktor Default (Percobaan 2) 
-        Mahasiswa25 mhs1 = new Mahasiswa25();
-        mhs1.nama = "Muhammad Ali Farhan"; 
-        mhs1.nim = "2241720171"; 
-        mhs1.kelas = "SI 2J"; 
-        mhs1.ipk = 3.55; 
-
-        System.out.println("--- Data Mahasiswa 1 ---");
-        mhs1.tampilkanInformasi(); 
-        
-        // Menguji Method Update
-        mhs1.ubahKelas("SI 2K"); 
-        mhs1.updateIpk(3.60); 
-        mhs1.tampilkanInformasi(); 
-
-        System.out.println("\n--- Data Mahasiswa 2 ---");
-        // Instansiasi menggunakan Konstruktor Berparameter (Percobaan 3) 
-        Mahasiswa25 mhs2 = new Mahasiswa25("Annisa Nabila", "2141720160", 3.25, "TI 2L");
-        mhs2.updateIpk(3.30); 
-        mhs2.tampilkanInformasi(); 
-        System.out.println("Hasil Evaluasi: " + mhs2.nilaiKinerja());
+        if (ipk >= 3.5) return "Kinerja sangat baik";
+        else if (ipk >= 3.0) return "Kinerja baik";
+        else if (ipk >= 2.0) return "Kinerja cukup";
+        else return "Kinerja kurang";
     }
 }
