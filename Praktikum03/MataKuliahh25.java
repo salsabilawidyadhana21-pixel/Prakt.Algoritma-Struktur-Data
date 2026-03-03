8fd9b10c61302aded5660d234d607edf91ed89f4
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-03-03 16:02:50 +0700

    Percobaan 3.4 MataKuliah25

---
+++ Praktikum03/MataKuliahh25.java
@@ -0,0 +1,14 @@
+public class MataKuliahh25 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    // Constructor berparameter sesuai langkah 1 
+    public MataKuliahh25(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode; 
+        this.nama = nama; 
+        this.sks = sks; 
+        this.jumlahJam = jumlahJam; 
+    }
+}
