dd2f89447051f678536debd7dbb04d48089ff6ae
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-03-03 15:42:42 +0700

    Percobaan 3.3 Modif Pertanyaan no.1 MahasiswaDemo25

--- Praktikum03/MahasiswaDemo25.java
+++ Praktikum03/MahasiswaDemo25.java
@@ -6,29 +6,30 @@ public class MahasiswaDemo25 {
         Mahasiswaa25[] arrayOfMahasiswa = new Mahasiswaa25[3];
         String dummy;
 
-        // Proses Input menggunakan Looping
+        // Proses Input Data menggunakan Looping (Percobaan 3.3)
         for (int i = 0; i < 3; i++) {
-            arrayOfMahasiswa[i] = new Mahasiswaa25(); // Instansiasi objek di setiap indeks 
+            arrayOfMahasiswa[i] = new Mahasiswaa25();
+            
             System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
             System.out.print("NIM    : ");
             arrayOfMahasiswa[i].nim = sc.nextLine();
+            
             System.out.print("Nama   : ");
             arrayOfMahasiswa[i].nama = sc.nextLine();
+            
             System.out.print("Kelas  : ");
             arrayOfMahasiswa[i].kelas = sc.nextLine();
+            
             System.out.print("IPK    : ");
             dummy = sc.nextLine();
-            arrayOfMahasiswa[i].ipk = Float.parseFloat(dummy); // Mengonversi input String ke Float 
+            arrayOfMahasiswa[i].ipk = Float.parseFloat(dummy);
             System.out.println("---------------------------");
         }
 
-        // Proses Menampilkan Data menggunakan Looping
+        // Proses Menampilkan Data menggunakan method cetakInfo() (Pertanyaan 3.3)
         for (int i = 0; i < 3; i++) {
             System.out.println("Data Mahasiswa ke-" + (i + 1));
-            System.out.println("NIM    : " + arrayOfMahasiswa[i].nim);
-            System.out.println("Nama   : " + arrayOfMahasiswa[i].nama);
-            System.out.println("Kelas  : " + arrayOfMahasiswa[i].kelas);
-            System.out.println("IPK    : " + arrayOfMahasiswa[i].ipk);
+            arrayOfMahasiswa[i].cetakInfo(); // Memanggil method dari class Mahasiswaa25
             System.out.println("---------------------------");
         }
     }
