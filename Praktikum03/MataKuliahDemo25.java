243a471f85f7269103163ea89797ce5d87acf836
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-03-03 16:34:51 +0700

    Jawaban No. 2,3,4 Pertanyaan 3.4.3

--- Praktikum03/MataKuliahDemo25.java
+++ Praktikum03/MataKuliahDemo25.java
@@ -1,40 +1,41 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class MataKuliahDemo25 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); 
-        MataKuliahh25[] arrayOfMatakuliah = new MataKuliahh25[3]; 
-        String kode, nama, dummy; 
-        int sks, jumlahJam; 
+        Scanner sc = new Scanner(System.in);
+        
+        // Jawaban No 4: Panjang array ditentukan oleh user melalui input Scanner 
+        System.out.print("Masukkan jumlah matakuliah: ");
+        int jumlahEksperimen = Integer.parseInt(sc.nextLine());
+        MataKuliahh25[] arrayOfMatakuliah = new MataKuliahh25[jumlahEksperimen];
 
-        // --- PROSES INPUT (Langkah 2) ---
-        for (int i = 0; i < 3; i++) { 
-            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1)); 
-            System.out.print("Kode       : "); 
-            kode = sc.nextLine(); 
-            System.out.print("Nama       : "); 
-            nama = sc.nextLine(); 
-            System.out.print("Sks        : "); 
-            dummy = sc.nextLine(); 
-            sks = Integer.parseInt(dummy); 
-            System.out.print("Jumlah Jam : "); 
-            dummy = sc.nextLine(); 
-            jumlahJam = Integer.parseInt(dummy); 
-            System.out.println("--------------------------------"); 
+        String kode, nama, dummy;
+        int sks, jumlahJam;
 
-            // Instansiasi menggunakan constructor berparameter
-            arrayOfMatakuliah[i] = new MataKuliahh25(kode, nama, sks, jumlahJam); 
+        // Proses Input menggunakan perulangan sesuai panjang array 
+        for (int i = 0; i < arrayOfMatakuliah.length; i++) {
+            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
+            System.out.print("Kode       : ");
+            kode = sc.nextLine();
+            System.out.print("Nama       : ");
+            nama = sc.nextLine();
+            System.out.print("Sks        : ");
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam : ");
+            dummy = sc.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("--------------------------------");
+
+            // Instansiasi objek dengan constructor berparameter 
+            arrayOfMatakuliah[i] = new MataKuliahh25(kode, nama, sks, jumlahJam);
         }
 
-        // --- PROSES MENAMPILKAN DATA (Langkah 4) ---
-        System.out.println("\n--- Hasil Input Data ---"); 
-        for (int i = 0; i < 3; i++) { 
-            System.out.println("Data Matakuliah ke-" + (i + 1)); 
-            System.out.println("Kode       : " + arrayOfMatakuliah[i].kode); 
-            System.out.println("Nama       : " + arrayOfMatakuliah[i].nama); 
-            System.out.println("Sks        : " + arrayOfMatakuliah[i].sks); 
-            System.out.println("Jumlah Jam : " + arrayOfMatakuliah[i].jumlahJam); 
-            System.out.println("--------------------------------"); 
+        // Jawaban No 3: Menampilkan data menggunakan method cetakInfo() 
+        System.out.println("\n--- DATA MATAKULIAH (HASIL CETAK INFO) ---");
+        for (int i = 0; i < arrayOfMatakuliah.length; i++) {
+            System.out.println("Data Matakuliah ke-" + (i + 1));
+            arrayOfMatakuliah[i].cetakInfo(); // Pemanggilan method dari class MataKuliahh25
         }
     }
 }
