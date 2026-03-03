04074b4bd1abe6809d8f0261cf01261332f344d9
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-03-03 15:41:34 +0700

    Percobaan 3.3 Modif Pertanyaan no.1 Mahasiswa25

--- Praktikum03/Mahasiswaa25.java
+++ Praktikum03/Mahasiswaa25.java
@@ -3,4 +3,12 @@ public class Mahasiswaa25 {
     public String nama;
     public String kelas;
     public float ipk;
+
+    // Method untuk menampilkan informasi mahasiswa
+    public void cetakInfo() {
+        System.out.println("NIM    : " + nim);
+        System.out.println("Nama   : " + nama);
+        System.out.println("Kelas  : " + kelas);
+        System.out.println("IPK    : " + ipk);
+    }
 }
