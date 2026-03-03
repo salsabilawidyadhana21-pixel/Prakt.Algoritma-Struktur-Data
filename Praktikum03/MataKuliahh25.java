1d13c39afa76430ea8a88aad5114f1fc841c5995
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-03-03 16:33:23 +0700

    Jawaban No. 1,2,3 Pertanyaan 3.4.3

--- Praktikum03/MataKuliahh25.java
+++ Praktikum03/MataKuliahh25.java
@@ -4,11 +4,31 @@ public class MataKuliahh25 {
     public int sks;
     public int jumlahJam;
 
-    // Constructor berparameter sesuai langkah 1 
+    // Jawaban No 1 (Contoh): Menambahkan lebih dari 1 constructor (Overloading) 
+    public MataKuliahh25() {
+    }
+
     public MataKuliahh25(String kode, String nama, int sks, int jumlahJam) {
-        this.kode = kode; 
-        this.nama = nama; 
-        this.sks = sks; 
-        this.jumlahJam = jumlahJam; 
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
+    // Jawaban No 2: Menambahkan method tambahData() 
+    public void tambahData(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
+    // Jawaban No 3: Menambahkan method cetakInfo() 
+    public void cetakInfo() {
+        System.out.println("Kode       : " + kode);
+        System.out.println("Nama       : " + nama);
+        System.out.println("Sks        : " + sks);
+        System.out.println("Jumlah Jam : " + jumlahJam);
+        System.out.println("--------------------------------");
     }
 }
