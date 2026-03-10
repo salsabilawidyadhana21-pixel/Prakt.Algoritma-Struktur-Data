7194fdfdcc9b44a3b42c598baf4b3c0a79f8e16f
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-03-10 16:43:24 +0700

    Jawaban No.3 5.3.3

--- BruteForceDivideConquer/Minggu5/MainPangkat25.java
+++ BruteForceDivideConquer/Minggu5/MainPangkat25.java
@@ -1,41 +1,36 @@
 
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class MainPangkat25 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); 
+        Scanner input = new Scanner(System.in);
         
-        System.out.println("------------------");
-        System.out.print("Masukkan jumlah elemen: "); 
-        int elemen = input.nextInt(); 
+        System.out.print("Masukkan jumlah elemen: ");
+        int elemen = input.nextInt();
 
-        // Instansiasi array of object 
         Pangkat25[] png = new Pangkat25[elemen];
 
-        // Input data basis dan pangkat 
         for (int i = 0; i < elemen; i++) {
-            System.out.print("Masukkan nilai basis elemen ke-" + (i + 1) + ": "); 
-            int basis = input.nextInt(); 
-            System.out.print("Masukkan nilai pangkat elemen ke-" + (i + 1) + ": "); 
-            int pangkat = input.nextInt(); 
-            png[i] = new Pangkat25(basis, pangkat); 
+            System.out.print("Masukkan nilai basis elemen ke-" + (i + 1) + ": ");
+            int basis = input.nextInt();
+            System.out.print("Masukkan nilai pangkat elemen ke-" + (i + 1) + ": ");
+            int pangkat = input.nextInt();
+            png[i] = new Pangkat25(basis, pangkat);
         }
 
-        // Output hasil Brute Force 
         System.out.println("------------------");
-        System.out.println("HASIL PANGKAT BRUTEFORCE:");
+        System.out.println("HASIL PANGKAT BRUTEFORCE (Tanpa Parameter):");
         for (int i = 0; i < elemen; i++) {
+            // Pemanggilan method tanpa memasukkan argumen di dalam kurung
             System.out.println("Hasil dari " + png[i].nilai + "^" + png[i].pangkat + " adalah: " 
-                               + png[i].pangkatBF(png[i].nilai, png[i].pangkat)); 
+                               + png[i].pangkatBF()); 
         }
 
-        // Output hasil Divide and Conquer 
         System.out.println("------------------");
         System.out.println("HASIL PANGKAT DIVIDE AND CONQUER:");
         for (int i = 0; i < elemen; i++) {
             System.out.println("Hasil dari " + png[i].nilai + "^" + png[i].pangkat + " adalah: " 
-                               + png[i].pangkatDC(png[i].nilai, png[i].pangkat)); 
+                               + png[i].pangkatDC(png[i].nilai, png[i].pangkat));
         }
-        System.out.println("------------------");
     }
 }
