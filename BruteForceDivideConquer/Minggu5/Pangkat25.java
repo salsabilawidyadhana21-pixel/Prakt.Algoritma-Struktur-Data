e9eee0047a77eedcc8a8366b04c45374c56a04dd
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-03-10 16:42:44 +0700

    Jawaban Np.3 5.3.3

--- BruteForceDivideConquer/Minggu5/Pangkat25.java
+++ BruteForceDivideConquer/Minggu5/Pangkat25.java
@@ -2,33 +2,30 @@
 public class Pangkat25 {
     public int nilai, pangkat;
 
-    // Konstruktor berparameter 
     public Pangkat25(int n, int p) {
-        nilai = n; 
-        pangkat = p; 
+        nilai = n;
+        pangkat = p;
     }
 
-    // Method menggunakan Brute Force 
-    public int pangkatBF(int a, int n) {
-        int hasil = 1; 
-        for (int i = 0; i < n; i++) { 
-            hasil = hasil * a; 
+    // Method PangkatBF tanpa parameter (Jawaban Pertanyaan No. 3)
+    public int pangkatBF() {
+        int hasil = 1;
+        for (int i = 0; i < pangkat; i++) {
+            hasil = hasil * nilai;
         }
-        return hasil; 
+        return hasil;
     }
 
-    // Method menggunakan Divide and Conquer 
+    // Method PangkatDC tetap menggunakan parameter karena rekursif
     public int pangkatDC(int a, int n) {
         if (n == 0) {
             return 1;
-        } else if (n == 1) { 
-            return a; 
+        } else if (n == 1) {
+            return a;
         } else {
-            if (n % 2 == 1) { 
-                // Kondisi Ganjil 
+            if (n % 2 == 1) {
                 return (pangkatDC(a, n / 2) * pangkatDC(a, n / 2) * a);
             } else {
-                // Kondisi Genap
                 return (pangkatDC(a, n / 2) * pangkatDC(a, n / 2));
             }
         }
