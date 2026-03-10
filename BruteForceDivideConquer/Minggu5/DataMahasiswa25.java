8b36f06c03442f3ceacbb8d3959f9bbe5e9f53e0
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-03-10 16:01:54 +0700

    DataMahasiswa25 Lat.Prakt.1 Jobsheet5

---
+++ BruteForceDivideConquer/Minggu5/DataMahasiswa25.java
@@ -0,0 +1,13 @@
+
+public class DataMahasiswa25 {
+    public String nama;
+    public int nim, tahunMasuk, nilaiUts, nilaiUas;
+
+    public DataMahasiswa25(String nama, int nim, int tahun, int uts, int uas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.tahunMasuk = tahun;
+        this.nilaiUts = uts;
+        this.nilaiUas = uas;
+    }
+}
