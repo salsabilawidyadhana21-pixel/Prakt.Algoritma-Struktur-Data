54b7627ba1ebeb15ca9ffaf0f9ef7ce913bb92c5
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-03-10 16:35:01 +0700

    Jawaban No.2 5.2.3

--- BruteForceDivideConquer/Minggu5/Faktorial25.java
+++ BruteForceDivideConquer/Minggu5/Faktorial25.java
@@ -1,19 +1,24 @@
 
 
 public class Faktorial25 {
+
+    // Method Brute Force menggunakan perulangan WHILE (Jawaban Pertanyaan No. 2)
     public int faktorialBF(int n) {
-        int fakto = 1; 
-        for (int i = 1; i <= n; i++) { 
-            fakto = fakto * i; 
+        int fakto = 1;
+        int i = 1;
+        while (i <= n) {
+            fakto = fakto * i;
+            i++;
         }
-        return fakto; 
+        return fakto;
     }
 
+    // Method Divide and Conquer tetap menggunakan Rekursif
     public int faktorialDC(int n) {
-        if (n == 1) { 
-            return 1; 
+        if (n == 1) {
+            return 1;
         } else {
-            return n * faktorialDC(n - 1); 
+            return n * faktorialDC(n - 1);
         }
     }
 }
