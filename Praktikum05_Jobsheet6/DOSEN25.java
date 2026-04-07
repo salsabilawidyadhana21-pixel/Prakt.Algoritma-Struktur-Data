4a4b902fee9b269332b595b9d7cd5c1a46a72223
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-04-07 19:38:56 +0700

    jawaban 5.5 dosen25

---
+++ Praktikum05_Jobsheet6/DOSEN25.java
@@ -0,0 +1,19 @@
+public class DOSEN25 {
+    String kode, nama;
+    boolean jenisKelamin; // true: Laki-laki, false: Perempuan
+    int usia;
+
+    public DOSEN25(String kd, String name, boolean jk, int age) {
+        kode = kd;
+        nama = name;
+        jenisKelamin = jk;
+        usia = age;
+    }
+
+    void tampil() {
+        System.out.println("Kode          : " + kode);
+        System.out.println("Nama          : " + nama);
+        System.out.println("Jenis Kelamin : " + (jenisKelamin ? "Laki-laki" : "Perempuan"));
+        System.out.println("Usia          : " + usia);
+    }
+}
