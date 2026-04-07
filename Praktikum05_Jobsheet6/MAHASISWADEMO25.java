fa3f6145d4d02e149d3119157cef3720e61dd0ba
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-04-07 18:17:59 +0700

    prakt.2 jawaban pertanyaan no.2 modif MahasiswaDemo25.java

--- Praktikum05_Jobsheet6/MAHASISWADEMO25.java
+++ Praktikum05_Jobsheet6/MAHASISWADEMO25.java
@@ -1,28 +1,35 @@
+import java.util.Scanner; 
+
 public class MAHASISWADEMO25 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         MAHASISWABERPRESTASI25 list = new MAHASISWABERPRESTASI25();
-        
-        MAHASISWA25 m1 = new MAHASISWA25("123", "Zidan", "2A", 3.2);
-        MAHASISWA25 m2 = new MAHASISWA25("124", "Ayu", "2A", 3.5);
-        MAHASISWA25 m3 = new MAHASISWA25("125", "Sofi", "2A", 3.1);
-        MAHASISWA25 m4 = new MAHASISWA25("126", "Sita", "2A", 3.9);
-        MAHASISWA25 m5 = new MAHASISWA25("127", "Miki", "2A", 3.7);
+        int jmlMhs = 5;
 
-        list.tambah(m1);
-        list.tambah(m2);
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
+        for (int i = 0; i < jmlMhs; i++) {
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
+            System.out.print("NIM   : ");
+            String nim = sc.nextLine();
+            System.out.print("Nama  : ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas : ");
+            String kelas = sc.nextLine();
+            System.out.print("IPK   : ");
+            double ipk = sc.nextDouble();
+            sc.nextLine(); // Pembersih buffer
 
-        System.out.println("Data mahasiswa sebelum sorting: ");
-        list.tampil();
+            MAHASISWA25 m = new MAHASISWA25(nim, nama, kelas, ipk);
+            list.tambah(m); 
+            System.out.println();
+        }
 
-        System.out.println("Data Mahasiswa setelah sorting (Bubble Sort - DESC): ");
+        System.out.println("Data sebelum sorting:");
+        list.tampil(); 
+
+        System.out.println("Setelah sorting (Bubble Sort):");
         list.bubbleSort();
         list.tampil();
-
-        System.out.println("Data Mahasiswa setelah sorting (Selection Sort - ASC): ");
-        list.selectionSort();
-        list.tampil();
+        
+        sc.close();
     }
 }
