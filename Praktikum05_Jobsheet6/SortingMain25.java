fc53c20e9da5ae257f4babec9d6a37c91bc0383a
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-04-07 17:16:13 +0700

    Prakt.1 "b" SortingMain25.java

--- Praktikum05_Jobsheet6/SortingMain25.java
+++ Praktikum05_Jobsheet6/SortingMain25.java
@@ -1,18 +1,22 @@
 public class SortingMain25 {
     public static void main(String[] args) {
-        // Deklarasi dan pengisian array
         int a[] = {20, 10, 2, 7, 12};
+        int b[] = {20, 10, 2, 7, 12}; 
 
-        // Instansiasi objek menggunakan class Sorting25
+        // Eksekusi Bubble Sort
         Sorting25 dataurut1 = new Sorting25(a, a.length);
-
-        // Menampilkan data sebelum diurutkan
         System.out.println("Data awal 1");
         dataurut1.tampil();
-
-        // Melakukan sorting dan menampilkan hasil
         dataurut1.bubbleSort();
         System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
         dataurut1.tampil();
+
+        // Eksekusi Selection Sort (Bagian b)
+        Sorting25 dataurut2 = new Sorting25(b, b.length);
+        System.out.println("\nData awal 2");
+        dataurut2.tampil();
+        dataurut2.selectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
     }
 }
