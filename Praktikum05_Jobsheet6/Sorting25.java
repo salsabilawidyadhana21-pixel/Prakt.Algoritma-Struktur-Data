4b8142ca049b52e3246208a0f445a891de7f6811
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-04-07 17:15:24 +0700

    Prakt.1 "b" sorting25.java

--- Praktikum05_Jobsheet6/Sorting25.java
+++ Praktikum05_Jobsheet6/Sorting25.java
@@ -2,7 +2,7 @@ public class Sorting25 {
     int[] data;
     int jumData;
 
-    // Konstruktor untuk inisialisasi data
+    // Konstruktor
     Sorting25(int Data[], int jmlDat) {
         jumData = jmlDat;
         data = new int[jmlDat];
@@ -11,12 +11,12 @@ public class Sorting25 {
         }
     }
 
-    // Method Bubble Sort (ascending)
+    // Method Bubble Sort (dari bagian a)
     void bubbleSort() {
         int temp = 0;
         for (int i = 0; i < jumData - 1; i++) {
             for (int j = 1; j < jumData - i; j++) {
-                if (data[j - 1] > data[j]) { // Proses Pertukaran
+                if (data[j - 1] > data[j]) {
                     temp = data[j];
                     data[j] = data[j - 1];
                     data[j - 1] = temp;
@@ -25,7 +25,23 @@ public class Sorting25 {
         }
     }
 
-    // Method untuk menampilkan isi array
+    // --- Method Selection Sort (Praktikum 1 bagian b) ---
+    void selectionSort() {
+        for (int i = 0; i < jumData - 1; i++) {
+            int minId = i;
+            for (int j = i + 1; j < jumData; j++) {
+                if (data[j] < data[minId]) {
+                    minId = j;
+                }
+            }
+            // Swap elemen terkecil dengan elemen di posisi i
+            int temp = data[minId];
+            data[minId] = data[i];
+            data[i] = temp;
+        }
+    }
+
+    // Method tampil data
     void tampil() {
         for (int i = 0; i < jumData; i++) {
             System.out.print(data[i] + " ");
