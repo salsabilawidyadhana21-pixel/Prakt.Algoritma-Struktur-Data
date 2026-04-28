a50fdd275e3574c8b9f59e4e8fec6605ad412dea
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-04-28 19:33:31 +0700

    Modifikasi pertanyaan percobaan 2 No.2

--- Jobsheet9/KonversiBiner_25.java
+++ Jobsheet9/KonversiBiner_25.java
@@ -2,72 +2,63 @@ import java.util.Scanner;
 
 // 1. Class Stack 
 class StackBiner_25 {
-    int size;
-    int top;
+    int size, top;
     int[] stack;
 
     public StackBiner_25(int size) {
         this.size = size;
         this.stack = new int[size];
-        this.top = -1;
+        this.top = -1; 
     }
 
-    public boolean isFull() {
-        return top == size - 1;
+    public void push(int data) {
+        if (top < size - 1) {
+            stack[++top] = data; 
+        }
+    }
+
+    public int pop() {
+        if (top > -1) {
+            return stack[top--]; 
+        }
+        return -1; 
     }
 
     public boolean isEmpty() {
         return top == -1;
     }
-
-    public void push(int data) {
-        if (!isFull()) {
-            top++;
-            stack[top] = data;
-        } else {
-            System.out.println("Stack Penuh!");
-        }
-    }
-
-    public int pop() {
-        if (!isEmpty()) {
-            int data = stack[top];
-            top--;
-            return data;
-        } else {
-            return -1;
-        }
-    }
 }
 
-// 2. Class Main 
+// 2. Class Main
 public class KonversiBiner_25 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        StackBiner_25 st = new StackBiner_25(32);
+        StackBiner_25 st = new StackBiner_25(32); 
 
         System.out.print("Masukkan bilangan desimal: ");
-        int desimal = sc.nextInt();
-        int temp = desimal;
+        int desimal = sc.nextInt(); 
+        int temp = desimal;         
 
-        // Proses pembagian dan Push 
-        System.out.println("\nProses pembagian:");
+        System.out.println("\nProses pembagian (PUSH ke Stack):");
+        if (temp == 0) {
+            st.push(0);
+        }
+        
+        // MODIFIKASI: Menampilkan alur pembagian No.2
         while (temp > 0) {
-            int sisa = temp % 2;
+            int sisa = temp % 2; // Operasi Modulo untuk mendapatkan angka biner (0 atau 1)
             System.out.println(temp + " / 2 = " + (temp/2) + " sisa " + sisa);
-            st.push(sisa);
-            temp = temp / 2;
+            
+            st.push(sisa); // Memasukkan sisa bagi ke stack (LIFO)
+            temp /= 2;     // Membagi angka desimal dengan 2 untuk iterasi berikutnya
         }
 
-        // Tampilan Hasil (Pop)
+        // Menampilkan hasil akhir dengan operasi POP
         System.out.print("\nHasil konversi biner dari " + desimal + " adalah: ");
-        if (desimal == 0) {
-            System.out.print(0);
-        } else {
-            while (!st.isEmpty()) {
-                System.out.print(st.pop());
-            }
+        while (!st.isEmpty()) {
+            // Data yang terakhir masuk (sisa terakhir) akan keluar duluan
+            System.out.print(st.pop()); 
         }
-        System.out.println();
+        System.out.println(); // Baris baru agar tampilan terminal rapi
     }
 }
