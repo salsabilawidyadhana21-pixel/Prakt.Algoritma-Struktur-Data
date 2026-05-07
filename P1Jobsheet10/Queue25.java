036c5ae5c792194202acec1b76a3436ab227816a
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-05-07 15:27:24 +0700

    Modifikasi percobaan 1 (Queue)

--- P1Jobsheet10/Queue25.java
+++ P1Jobsheet10/Queue25.java
@@ -1,10 +1,11 @@
 public class Queue25 {
     int[] data;
-    int front;
-    int rear;
-    int size;
-    int max;
+    int front; 
+    int rear;  
+    int size;  
+    int max;   
 
+    
     public Queue25(int n) {
         max = n;
         data = new int[max];
@@ -13,62 +14,27 @@ public class Queue25 {
         rear = -1;
     }
 
+    
     public boolean isEmpty() {
-        if (size == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return size == 0;
     }
 
+    
     public boolean isFull() {
-        if (size == max) {
-            return true;
-        } else {
-            return false;
-        }
+        return size == max;
     }
 
-    public void peek() {
-        if (!isEmpty()) {
-            System.out.println("Elemen terdepan: " + data[front]);
-        } else {
-            System.out.println("Queue masih kosong");
-        }
-    }
-
-    public void print() {
-        if (isEmpty()) {
-            System.out.println("Queue masih kosong");
-        } else {
-            int i = front;
-            while (i != rear) {
-                System.out.print(data[i] + " ");
-                i = (i + 1) % max;
-            }
-            System.out.println(data[i] + " ");
-            System.out.println("Jumlah elemen = " + size);
-        }
-    }
-
-    public void clear() {
-        if (!isEmpty()) {
-            front = 0;
-            rear = -1;
-            size = 0;
-            System.out.println("Queue berhasil dikosongkan");
-        } else {
-            System.out.println("Queue masih kosong");
-        }
-    }
-
+    
     public void enqueue(int dt) {
         if (isFull()) {
-            System.out.println("Queue sudah penuh");
+            // Modifikasi: Program berhenti jika terjadi Overflow
+            System.out.println("Queue sudah penuh (Overflow)! Program dihentikan.");
+            System.exit(0); 
         } else {
             if (isEmpty()) {
                 front = rear = 0;
             } else {
+                
                 if (rear == max - 1) {
                     rear = 0;
                 } else {
@@ -80,16 +46,21 @@ public class Queue25 {
         }
     }
 
+    
     public int dequeue() {
         int dt = 0;
         if (isEmpty()) {
-            System.out.println("Queue masih kosong");
+            // Modifikasi: Program berhenti jika terjadi Underflow
+            System.out.println("Queue masih kosong (Underflow)! Program dihentikan.");
+            System.exit(0);
+            return 0; 
         } else {
             dt = data[front];
             size--;
             if (isEmpty()) {
                 front = rear = -1;
             } else {
+                
                 if (front == max - 1) {
                     front = 0;
                 } else {
@@ -98,5 +69,37 @@ public class Queue25 {
             }
         }
         return dt;
+    }
+
+    
+    public void peek() {
+        if (!isEmpty()) {
+            System.out.println("Elemen terdepan: " + data[front]);
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+
+    
+    public void print() {
+        if (isEmpty()) {
+            System.out.println("Queue masih kosong");
+        } else {
+            int i = front;
+            while (i != rear) {
+                System.out.print(data[i] + " ");
+                i = (i + 1) % max; 
+            }
+            System.out.println(data[i] + " ");
+            System.out.println("Jumlah elemen = " + size);
+        }
+    }
+
+    
+    public void clear() {
+        front = 0;
+        rear = -1;
+        size = 0;
+        System.out.println("Queue berhasil dikosongkan");
     }
 }
