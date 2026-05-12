cc2455c074b2dd9e47e3eb505894523ebe25a5ba
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-05-12 15:57:04 +0700

    class node antrian (tugas)

---
+++ Pertemuan12/NodeAntrian25.java
@@ -0,0 +1,9 @@
+public class NodeAntrian25 {
+    mAHASISWA25 data;
+    NodeAntrian25 next;
+
+    public NodeAntrian25(mAHASISWA25 data, NodeAntrian25 next) {
+        this.data = data;
+        this.next = next;
+    }
+}
