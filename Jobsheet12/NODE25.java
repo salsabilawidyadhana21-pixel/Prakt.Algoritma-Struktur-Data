aeb5617193df980535787f9302d8a403af1fdd82
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-05-12 17:44:19 +0700

    class node perc.1

---
+++ Jobsheet12/NODE25.java
@@ -0,0 +1,11 @@
+public class NODE25 {
+    mahasiswa25 data;
+    NODE25 prev;
+    NODE25 next;
+
+    public NODE25(mahasiswa25 data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
