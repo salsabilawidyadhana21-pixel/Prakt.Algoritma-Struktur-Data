280eda5ea312d5a4d5e3597e8acc7c22c019fe4e
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-05-12 18:18:25 +0700

    Class DLL Main perc.2 + modif

--- Jobsheet12/DoubleLinkedListMain25.java
+++ Jobsheet12/DoubleLinkedListMain25.java
@@ -11,6 +11,8 @@ public class DoubleLinkedListMain25 {
             System.out.println("1. Tambah data di awal");
             System.out.println("2. Tambah data di akhir");
             System.out.println("3. Sisipkan data di tengah (setelah NIM)");
+            System.out.println("4. Hapus data di awal");
+            System.out.println("5. Hapus data di akhir");
             System.out.println("6. Tampilkan data");
             System.out.println("0. Keluar");
             System.out.print("Pilih menu: ");
@@ -25,19 +27,24 @@ public class DoubleLinkedListMain25 {
                     list.addLast(inputMahasiswa(scan));
                     break;
                 case 3:
-                    System.out.print("Masukkan NIM yang dicari: ");
+                    System.out.print("Masukkan NIM acuan: ");
                     String keyNim = scan.nextLine();
-                    System.out.println("Masukkan data baru: ");
                     list.insertAfter(keyNim, inputMahasiswa(scan));
+                    break;
+                case 4:
+                    list.removeFirst();
+                    break;
+                case 5:
+                    list.removeLast();
                     break;
                 case 6:
                     list.print();
                     break;
                 case 0:
-                    System.out.println("Program selesai.");
+                    System.out.println("Program Selesai.");
                     break;
                 default:
-                    System.out.println("Menu tidak valid.");
+                    System.out.println("Pilihan tidak valid.");
             }
         } while (pilihan != 0);
         scan.close();
@@ -52,7 +59,7 @@ public class DoubleLinkedListMain25 {
         String kelas = scan.nextLine();
         System.out.print("Masukkan IPK   : ");
         double ipk = scan.nextDouble();
-        scan.nextLine();
+        scan.nextLine(); 
         return new mahasiswa25(nim, nama, kelas, ipk);
     }
 }
