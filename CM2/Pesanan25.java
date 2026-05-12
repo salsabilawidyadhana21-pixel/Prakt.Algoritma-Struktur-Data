0934fb3a09e40cc79a35c4cb14d960df94e524b0
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-05-12 16:54:56 +0700

    Class Pesanan (CM2)

---
+++ CM2/Pesanan25.java
@@ -0,0 +1,11 @@
+public class Pesanan25 {
+    int kodePesanan;
+    String namaPesanan;
+    int harga;
+
+    public Pesanan25(int a, String b, int d) {
+        this.kodePesanan = a;
+        this.namaPesanan = b;
+        this.harga = d;
+    }
+}
