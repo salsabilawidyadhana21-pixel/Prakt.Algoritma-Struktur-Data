7e4b69e48d96d60e9644bf23fe81b740ff501c09
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-06-02 18:52:30 +0700

    tugas prakt. class MahasiswaTugas (lengkap no.1 & 2)

---
+++ Jobsheet16Collection/MahasiswaTugas.java
@@ -0,0 +1,13 @@
+// Class untuk menyimpan data objek Mahasiswa
+public class MahasiswaTugas {
+    String nim;
+    String nama;
+    String telf;
+
+    // Konstruktor untuk menginisialisasi data mahasiswa
+    public MahasiswaTugas(String nim, String nama, String telf) {
+        this.nim = nim;
+        this.nama = nama;
+        this.telf = telf;
+    }
+}
