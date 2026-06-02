647a3671de1d0cf2a277ac7594f27c7776257587
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-06-02 18:53:12 +0700

    tugas prakt. class MataKuliahTugas 9lengkap no.1 & 2)

---
+++ Jobsheet16Collection/MataKuliahTugas.java
@@ -0,0 +1,13 @@
+// Class untuk menyimpan data objek Mata Kuliah
+public class MataKuliahTugas {
+    String kode;
+    String namaMk;
+    int sks;
+
+    // Konstruktor untuk menginisialisasi data mata kuliah
+    public MataKuliahTugas(String kode, String namaMk, int sks) {
+        this.kode = kode;
+        this.namaMk = namaMk;
+        this.sks = sks;
+    }
+}
