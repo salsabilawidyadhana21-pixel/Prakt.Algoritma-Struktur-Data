f1d1fee9cfb7e5e970fa1dfcea6b57511b2c9ed0
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-06-22 13:18:58 +0700

    percobaan 1 Mahasiswa

---
+++ Jobsheet14/Mahasiswa00.java
@@ -0,0 +1,25 @@
+public class Mahasiswa00 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    public Mahasiswa00() {
+
+    }
+
+    public Mahasiswa00(String nim, String nama, String kelas, double ipk) {
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+        this.ipk = ipk;
+    }
+
+    public void tampilInformasi() {
+        System.out.println("NIM   : " + nim);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
+        System.out.println("----------------------");
+    }
+}
