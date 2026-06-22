3809b5b6732dfe8324d1a2dacaa57b0516bd3ef6
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-06-22 13:45:15 +0700

    tugas praktikum binary tree array

--- Jobsheet14/BinaryTreeArray00.java
+++ Jobsheet14/BinaryTreeArray00.java
@@ -1,27 +1,45 @@
 public class BinaryTreeArray00 {
+
     Mahasiswa00[] data;
     int idxLast;
 
     public BinaryTreeArray00() {
-        data = new Mahasiswa00[10];
+        data = new Mahasiswa00[20];
+        idxLast = -1;
     }
 
-    public void populateData(Mahasiswa00[] data, int idxLast) {
-        this.data = data;
-        this.idxLast = idxLast;
+    public void add(Mahasiswa00 mhs) {
+
+        idxLast++;
+
+        if (idxLast < data.length) {
+            data[idxLast] = mhs;
+        } else {
+            System.out.println("Array penuh!");
+        }
     }
 
     public void traverseInOrder(int idxStart) {
-        if (idxStart <= idxLast) {
 
-            if (data[idxStart] != null) {
+        if (idxStart <= idxLast && data[idxStart] != null) {
 
-                traverseInOrder(2 * idxStart + 1);
+            traverseInOrder(2 * idxStart + 1);
 
-                data[idxStart].tampilInformasi();
+            data[idxStart].tampilInformasi();
 
-                traverseInOrder(2 * idxStart + 2);
-            }
+            traverseInOrder(2 * idxStart + 2);
+        }
+    }
+
+    public void traversePreOrder(int idxStart) {
+
+        if (idxStart <= idxLast && data[idxStart] != null) {
+
+            data[idxStart].tampilInformasi();
+
+            traversePreOrder(2 * idxStart + 1);
+
+            traversePreOrder(2 * idxStart + 2);
         }
     }
 }
